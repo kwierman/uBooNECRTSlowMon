3b650f981eb6acc8fefb372f4c99a86103cb8143
Kevin Wierman <kwierman@gmail.com>
2016-12-28 14:39:57 -0600

    Woops part 5

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import find_packages
 from distutils.core import setup
 from pip.req import parse_requirements
 
-install_reqs = parse_requirements("requirements.txt")
-reqs = [str(ir.req) for ir in install_reqs]
-
 setup(name='uBooNECRTSlowMon',
       version='1.0',
       description='MicroBooNE CRT Slow Controls Monitoring Script',
