a4802c7bfebe9353a40dcbcafd36f231e7b9279d
Kevin Wierman <kwierman@gmail.com>
2016-12-28 14:19:35 -0600

    Adding in distutils

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='uBooNECRTSlowMon',
+      version='1.0',
+      description='MicroBooNE CRT Slow Controls Monitoring Script',
+      author='Kevin Wierman',
+      author_email='kwierman@fnal.gov',
+      url='https://github.com/kwierman/uBooNECRTSlowMon',
+      packages=['SCMon'],
+     )
