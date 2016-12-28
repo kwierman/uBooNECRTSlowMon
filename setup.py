9c8f5dc2d0696914c5c9eb3029ec6371dc173ea7
Kevin Wierman <kwierman@gmail.com>
2016-12-28 14:40:47 -0600

    Woops part 6

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 from setuptools import find_packages
 from distutils.core import setup
 from pip.req import parse_requirements
+import pip
+
+install_reqs = parse_requirements("requirements.txt", session=pip.download.PipSession())
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='uBooNECRTSlowMon',
       version='1.0',
@@ -9,5 +13,6 @@ setup(name='uBooNECRTSlowMon',
       author='Kevin Wierman',
       author_email='kwierman@fnal.gov',
       url='https://github.com/kwierman/uBooNECRTSlowMon',
+      requires = reqs,
       packages=find_packages(exclude=[]),
      )
