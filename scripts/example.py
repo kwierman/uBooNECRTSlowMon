0ed82525b19d8175ee8df1bdef2437c1117d4242
Kevin Wierman <kwierman@gmail.com>
2016-12-28 15:35:53 -0600

    Adding more stuff to example

--- scripts/example.py
+++ scripts/example.py
@@ -8,15 +8,20 @@ from SCMon import (MessageQuery,
 client = MessageQuery._client
 
 
-messages = MessageQuery(client, column='*', constraints='time > now() - 1d', limit=1000)
+messages = MessageQuery(client, column='*', constraints='time > now() - 1d', limit=100)
 print messages.constuct_query()
 
-messages = MessageQuery(client, column='text', constraints='time > now() - 1d', limit=1000)
+messages = MessageQuery(client, column='text', constraints='time > now() - 1d', limit=100)
 print messages.constuct_query()
 
-messages = MessageQuery(client, column='title', constraints='time > now() - 1d', limit=1000)
+messages = MessageQuery(client, column='title', constraints='time > now() - 1d', limit=100)
 print messages.constuct_query()
 
-
-feb_stats = FEBStatsQuery(client, column='*', constraints='time > now() - 1d', limit=1000)
+feb_stats = FEBStatsQuery(client, column='*', constraints='time > now() - 1d', limit=100)
 print feb_stats.constuct_query()
+
+drv_stats = DRVStatsQuery(client, column='*', constraints='time > now() - 1d', limit=100)
+print drv_stats.constuct_query()
+
+events = EventsQuery(client, column='*', constraints='time > now() - 1d', limit=100)
+print events.constuct_query()
