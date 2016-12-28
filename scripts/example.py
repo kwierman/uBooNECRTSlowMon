da70c1429908a0e08cc6a501a53fbb0dfa4db4a8
Kevin Wierman <kwierman@gmail.com>
2016-12-28 15:30:46 -0600

    Adding more stuff to example

--- scripts/example.py
+++ scripts/example.py
@@ -9,5 +9,14 @@ client = MessageQuery._client
 
 
 messages = MessageQuery(client, column='*', constraints='time > now() - 1d', limit=1000)
-print messages.list_series()
 print messages.constuct_query()
+
+messages = MessageQuery(client, column='text', constraints='time > now() - 1d', limit=1000)
+print messages.constuct_query()
+
+messages = MessageQuery(client, column='title', constraints='time > now() - 1d', limit=1000)
+print messages.constuct_query()
+
+
+feb_stats = FEBStatsQuery(client, column='*', constraints='time > now() - 1d', limit=1000)
+print feb_stats.constuct_query()
