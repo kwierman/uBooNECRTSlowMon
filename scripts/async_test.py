21f1321750c986551f71ddffdde3171e4d09b5a5
Kevin Wierman <kwierman@gmail.com>
2017-01-02 11:38:15 -0600

    Bugfix

--- scripts/async_test.py
+++ scripts/async_test.py
@@ -1,15 +1,8 @@
-import logging
 from daemon import runner
 from SCMon.app import App
 from SCMon import settings
 
 app = App()
-logger = logging.getLogger("DaemonLog")
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler = logging.FileHandler(settings.LOG_PATH)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 daemon_runner = runner.DaemonRunner(app)
 daemon_runner.daemon_context.files_preserve=[handler.stream]
 daemon_runner.do_action()
