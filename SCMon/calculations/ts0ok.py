2cf136ee31631775558d5ed227b0d12357e2e782
Kevin Wierman <kwierman@gmail.com>
2017-01-09 13:46:33 -0600

    Removing unnecessary lines

--- SCMon/calculations/ts0ok.py
+++ SCMon/calculations/ts0ok.py
@@ -4,4 +4,3 @@ from basets import BaseTSQuery
 class TS0OKQuery(BaseTSQuery):
   path=sc_set.TS0OK_PATH
   logger = logging.getLogger(__name__)
-  febs = sc_set.FT_FEBS+sc_set.BOTTOM_FEBS+sc_set.PIPE_FEBS+sc_set.TOP_FEBS
--- SCMon/calculations/ts1ok.py
+++ SCMon/calculations/ts1ok.py
@@ -4,4 +4,3 @@ from basets import BaseTSQuery
 class TS1OKQuery(BaseTSQuery):
   path=sc_set.TS1OK_PATH  
   logger = logging.getLogger(__name__)
-  febs = sc_set.FT_FEBS+sc_set.BOTTOM_FEBS+sc_set.PIPE_FEBS+sc_set.TOP_FEBS
