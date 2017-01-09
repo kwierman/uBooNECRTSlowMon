92780631ff620a8e9b678bde299f86d70d60a13b
Kevin Wierman <kwierman@gmail.com>
2017-01-09 13:49:11 -0600

    Bugfix

--- SCMon/calculations/ts0ok.py
+++ SCMon/calculations/ts0ok.py
@@ -5,3 +5,4 @@ from SCMon import settings as sc_set
 class TS0OKQuery(BaseTSQuery):
   path=sc_set.TS0OK_PATH
   logger = logging.getLogger(__name__)
+  updates=[]
--- SCMon/calculations/ts1ok.py
+++ SCMon/calculations/ts1ok.py
@@ -5,3 +5,4 @@ from SCMon import settings as sc_set
 class TS1OKQuery(BaseTSQuery):
   path=sc_set.TS1OK_PATH  
   logger = logging.getLogger(__name__)
+  updates=[]
